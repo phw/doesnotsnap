6e1ab1a6d817b7e49121009583b0ba4a464262d6
Philipp Wolfer <ph.wolfer@gmail.com>
2020-12-08 11:58:16 +0100

    Simplify app to a more minimal version that does not run

--- doesnotsnap/__init__.py
+++ doesnotsnap/__init__.py
@@ -16,25 +16,15 @@ class MainWindow(QtWidgets.QMainWindow):
     pass
 
 
-class WebService(QtCore.QObject):
-
-    def __init__(self, parent=None) -> None:
-        super().__init__(parent=parent)
-        print("WebService: Before QNetworkAccessManager")
-        self.manager = QtNetwork.QNetworkAccessManager()
-        print("WebService: After QNetworkAccessManager")
-
-
 class Application(QtWidgets.QApplication):
     def __init__(self, argv: typing.List[str]) -> None:
         print("PyQt %s" % pyqt_version)
         print("Qt %s" % qVersion())
         super().__init__(argv)
-        self.window = MainWindow()
         print("Application: Before QNetworkAccessManager")
-        self.manager = QtNetwork.QNetworkAccessManager()
+        self.manager = QtNetwork.QNetworkAccessManager(self)
         print("Application: After QNetworkAccessManager")
-        self.webservice = WebService()
+        self.window = MainWindow()
 
     def run(self):
         self.window.show()
